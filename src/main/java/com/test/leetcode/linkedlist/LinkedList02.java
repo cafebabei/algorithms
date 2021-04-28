7170f3d8c54f01bc31cf8c496a12101649cdfe85
fishsup <azuremem@gmail.com>
2021-04-28 16:02:37 +0800

    递归合并两个有序单链表

--- src/main/java/com/test/leetcode/linkedlist/LinkedList02.java
+++ src/main/java/com/test/leetcode/linkedlist/LinkedList02.java
@@ -54,22 +54,6 @@ public class LinkedList02 {
      * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode a = head;
